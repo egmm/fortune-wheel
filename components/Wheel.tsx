961fc715f1a4ec988be23af568a6436e7be88506
Ernesto <egmm.566@gmail.com>
2025-02-22 21:20:35 +0000

    Add indicator to Wheel component

--- components/Wheel.tsx
+++ components/Wheel.tsx
@@ -17,22 +17,29 @@ export const Wheel = ({ rotation, segments, segmentAngle }: Props) => {
   const colors = generateColors(segments.value.length);
   return segments.value.length > 0
     ? (
-      <div
-        id="wheel"
-        className="relative w-96 h-96 border-4 border-gray-300 rounded-full overflow-hidden transition-transform ease-out duration-[3000ms]"
-        style={{ transform: `rotate(${rotation.value}deg)` }}
-      >
-        {segments.value.map((segment, index) => (
-          <div
-            className="absolute w-1/2 h-1/2 origin-bottom-right clip-wedge flex items-center justify-center"
-            style={{
-              transform: `rotate(${index * segmentAngle}deg)`,
-              backgroundColor: colors[index],
-            }}
-          >
-            <span className="rotate-[45deg]">{segment}</span>
-          </div>
-        ))}
+      <div className="relative w-full h-1/2 pt-24 flex items-center justify-center">
+        <div
+          id="indicator"
+          className="absolute w-12 h-12 top-12 left-1/2 -ml-6 bg-slate-900 clip-triangle"
+        >
+        </div>
+        <div
+          id="wheel"
+          className="relative w-96 h-96 border-4 border-gray-300 rounded-full overflow-hidden transition-transform ease-out duration-[3000ms]"
+          style={{ transform: `rotate(${rotation.value}deg)` }}
+        >
+          {segments.value.map((segment, index) => (
+            <div
+              className="absolute w-1/2 h-1/2 origin-bottom-right clip-wedge flex items-center justify-center"
+              style={{
+                transform: `rotate(${index * segmentAngle}deg)`,
+                backgroundColor: colors[index],
+              }}
+            >
+              <span className="rotate-[45deg]">{segment}</span>
+            </div>
+          ))}
+        </div>
       </div>
     )
     : null;
