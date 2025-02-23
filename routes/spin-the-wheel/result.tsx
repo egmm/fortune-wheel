d07fee3865feab07910668ff451c403023731209
Ernesto <egmm.566@gmail.com>
2025-02-23 22:26:28 +0000

    Enhance result display with improved layout and styling

--- routes/spin-the-wheel/result.tsx
+++ routes/spin-the-wheel/result.tsx
@@ -3,5 +3,11 @@ import { PageProps } from "$fresh/server.ts";
 export default function Result(props: PageProps) {
   const url = new URL(props.url);
   const segment = url.searchParams.get("segment");
-  return <h1>Result is {segment}</h1>;
+  return (
+    <div className="pt-24 flex flex-col items-center bg-background text-white">
+      <h2 className="text-2xl">Congratulations! 🎉</h2>
+      <h3 className="text-lg">You are watching:</h3>
+      <h1 className="pt-12 text-6xl text-center">{segment}</h1>
+    </div>
+  );
 }
